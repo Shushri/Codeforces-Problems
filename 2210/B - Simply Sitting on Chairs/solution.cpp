eef5a666a91cd5c05fc78eaab8d3e9250cb91542
Shrishti Mishra <shubhiarnm@gmail.com>
2026-03-29 20:24:53 +0530

    Add Simply Sitting on Chairs [B] from Codeforces

---
+++ 2210/B - Simply Sitting on Chairs/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main(){    ios_base::sync_with_stdio(false);    cin.tie(NULL);        int t;    cin >> t;    while(t--){        int n;        cin >> n;        int ans = 0;        for(int i = 1; i <= n; i++){            int p;            cin >> p;            if(p <= i) ans++;        }        cout << ans << endl;    }    return 0;}
