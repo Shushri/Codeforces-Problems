1d1a9b8e622569ee0d0390cff4365de610910848
Shrishti Mishra <shubhiarnm@gmail.com>
2026-03-29 20:19:25 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -0,0 +1,1 @@
+#include<bits/stdc++.h>using namespace std; int main(){    int t;    cin >> t;    while(t--){        int n;        cin >> n;        cout << 1 << " " << n << " ";        for(int i = n-1; i >= 2; i--){            cout << i;            if(i > 2) cout << " ";        }        cout << endl;    }    return 0;}
